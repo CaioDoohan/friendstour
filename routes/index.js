5819018a9b73b2b5de37823f9a02eea9fa0f2f14
Rafael Boschini <rafaelboschini@gmail.com>
2017-12-12 21:23:14 -0200

    Ajustes rotas e HTML

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Friendstour - Home' });
 });
 
+/* GET empresa. */
+router.get('/empresa', function(req, res, next) {
+  res.render('empresa', { title: 'Friendstour - Empresa' });
+});
+
+/* GET lista. */
+router.get('/lista', function(req, res, next) {
+  res.render('lista', { title: 'Friendstour - Lista' });
+});
+
+
 module.exports = router;
