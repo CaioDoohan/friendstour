59fef2e4b84f2008fdfeb49d094166e72630014e
Caio Doohan <pereiradoohan@hotmail.com>
2017-12-27 08:52:35 -0200

    Index Desktop:OK

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    $(".item").hover(function(){
-
+    $(".carhome").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots:true, 
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $(".carvg").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        variableWidth: true,
     });
 });
