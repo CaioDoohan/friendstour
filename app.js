150d6a5217e4fc53cdfb3b3d804e25d2727836a7
Caio Doohan <pereiradoohan@hotmail.com>
2018-02-20 15:31:28 -0300

    Ok

--- app.js
+++ app.js
@@ -110,18 +110,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(8080, function(){
 //   console.log(' ');  
 //   console.log(chalk.green("                 /         / "));
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 exports.mysqlOptions = {
-                host: 'mysql427.umbler.com',
-                user: 'friends',
-                password: 'minhamae2311',
+                host: 'localhost',
+                user: 'root',
+                password: 'm10d12',
                 database: 'friendsdb',
                 socketPath: false,
                 connectionLimit: 5,
